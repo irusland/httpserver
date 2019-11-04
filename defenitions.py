549f4f2d3be6c76056290c4502522ad349e08f3f
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2019-11-04 19:53:38 +0500

    Tests, paths

---
+++ defenitions.py
@@ -0,0 +1,7 @@
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(ROOT_DIR, 'config.json')
+TESTS_DIR = os.path.join(ROOT_DIR, 'tests')
+QUERIES_DIR = os.path.join(TESTS_DIR, 'queries')
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,15 +1,16 @@
 import json
-import unittest
-import threading
 import multiprocessing
 import os
+import unittest
+
 
 from httpserver import Server
+from defenitions import CONFIG_PATH, QUERIES_DIR
 
 
 class MyTestCase(unittest.TestCase):
     def server_run(self):
-        with open('config.json') as cfg:
+        with open(CONFIG_PATH) as cfg:
             data = json.load(cfg)
         server = Server(data['host'], data['port'], data['server'], log=False)
         with server as s:
@@ -19,15 +20,15 @@ class MyTestCase(unittest.TestCase):
         server = multiprocessing.Process(target=self.server_run)
         server.start()
 
-        with open('get.txt') as cmdfile:
+        with open(os.path.join(QUERIES_DIR, 'get.txt')) as cmdfile:
             cmd = cmdfile.read()
             with os.popen(cmd) as outfile:
                 out = outfile.read()
-                with open('get_empty_res.txt') as resfile:
+                server.terminate()
+                with open(os.path.join(QUERIES_DIR, 'get_empty_res.txt')) \
+                        as resfile:
                     res = resfile.read()
-                    self.assertEqual(out, res)
-
-        server.terminate()
+                    self.assertMultiLineEqual(out, res)
 
 
 if __name__ == '__main__':
