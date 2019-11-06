198d472f7e65672c297fa9c34cf879e8a7ebbc7a
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2019-11-07 02:04:28 +0500

    Regex fixed, tests added

--- ruler.py
+++ ruler.py
@@ -20,40 +20,46 @@ class Ruler:
                 return {}
             return rules
 
-    def get_destination(self, url, raw, abs=True):
-        for key, path in raw.items():
+    def get_destination(self, url, rules, abs=True):
+        for key, path in rules.items():
             if abs:
                 path = self.to_abs_path(path)
+            # todo relative path
             # else:
             #     path = os.path.join('.', path)
             template = self.to_template(key)
-            match = re.match(template, url)
+            match = re.fullmatch(template, url)
             if match:
+                if os.path.isdir(path):
+                    path = os.path.join(path, f'{match["name"]}.'
+                                              f'{match["ext"]}')
+                print('debug', ' match: checking file',
+                      match.groupdict(), template, url, path)
                 if os.path.isfile(path):
                     return path
-                elif os.path.isdir(path):
-                    file_path = os.path.join(path, f'{match.group("name")}.'
-                                                   f'{match.group("ext")}')
-                    if os.path.isfile(file_path):
-                        return file_path
                 raise FileNotFoundError(path, url, template, abs)
+        raise FileNotFoundError(url, rules)
 
     def to_abs_path(self, path):
         return os.path.join(ROOT_DIR, path)
 
     def to_template(self, key):
-        r = re.compile(r'(?P<p>.*/)(?P<n>.*)\.(?P<e>.*)')
-        m = re.match(r, key)
+        # r_old = re.compile(r'(?P<path>.*/)(?P<name>.*)\.(?P<ext>.*)')
+        reg = re.compile(r'(?:(?P<path>.*/))*(?:(?P<name>.*)\.(?P<ext>.*))*')
+        match = re.match(reg, key)
+        if not match["name"]:
+            return re.compile(rf'(?P<path>{match["path"]})')
         return \
-            re.compile(rf'{m.group("p")}'
+            re.compile(rf'(?P<path>{match["path"]})'
+                       rf'(?!.*?/)'
                        rf'(?P<name>'
-                       rf'{".*" if m.group("n") == "*" else m.group("n")})'
+                       rf'{".*" if match["name"] == "*" else match["name"]})'
                        rf'\.(?P<ext>'
-                       rf'{".*" if m.group("e") == "*" else m.group("e")})')
+                       rf'{".*" if match["ext"] == "*" else match["ext"]})')
 
 
 if __name__ == '__main__':
     ruler = Ruler()
     r = ruler.get_rules()
-    d = ruler.get_destination('/3.html', r)
+    d = ruler.get_destination('/naksdasd.py', r)
     print(d)
--- tests/test_ruler.py
+++ tests/test_ruler.py
@@ -3,33 +3,78 @@ from ruler import Ruler
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.ruler = Ruler()
+        self.rules = self.ruler.get_rules()
+
+    def assertDestinationsEqual(self, url, path, rules=None):
+        if rules is None:
+            rules = self.rules
+        d = self.ruler.get_destination(url, rules, abs=True)
+        self.assertEqual(path, d)
+
     def test_get_abs_path_for_file(self):
-        ruler = Ruler()
-        r = ruler.get_rules()
-        d = ruler.get_destination('/index.html', r, abs=True)
-        self.assertEqual('/Users/irusland/Desktop/UrFU/'
-                         'python/httpserver/tmp/index.html', d)
+        self.assertDestinationsEqual(
+            '/index.html',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/index.html')
 
     def test_path_for_file_by_rule(self):
-        ruler = Ruler()
-        r = ruler.get_rules()
-        d = ruler.get_destination('/2.html', r, abs=True)
-        self.assertEqual('/Users/irusland/Desktop/UrFU/'
-                         'python/httpserver/tmp/pages/2.html', d)
+        self.assertDestinationsEqual(
+            '/2.html',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/pages/2.html')
 
     def test_path_for_file_by_any_rule(self):
-        ruler = Ruler()
-        r = ruler.get_rules()
-        d = ruler.get_destination('/any.html', r, abs=True)
-        self.assertEqual('/Users/irusland/Desktop/UrFU/'
-                         'python/httpserver/tmp/pages/any.html', d)
+        self.assertDestinationsEqual(
+            '/any.html',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/pages/any.html')
 
     def test_path_for_file_in_dir(self):
+        self.assertDestinationsEqual(
+            '/123.css',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/pages/css/123.css')
+
+    def test_file_not_found_with_rule(self):
         ruler = Ruler()
         r = ruler.get_rules()
-        d = ruler.get_destination('/123.css', r, abs=True)
-        self.assertEqual('/Users/irusland/Desktop/UrFU/'
-                         'python/httpserver/tmp/pages/css/123.css', d)
+        self.assertRaises(FileNotFoundError,
+                          ruler.get_destination,
+                          *('/no.css', r, True))
+
+    def test_file_exists_no_rule(self):
+        ruler = Ruler()
+        r = ruler.get_rules()
+        self.assertRaises(FileNotFoundError,
+                          ruler.get_destination,
+                          *('/norulefile.py', r, True))
+
+    def test_no_file_no_rule(self):
+        ruler = Ruler()
+        r = ruler.get_rules()
+        self.assertRaises(FileNotFoundError,
+                          ruler.get_destination,
+                          *('/foo.bar', r, True))
+
+    def test_name_with_dots(self):
+        self.assertDestinationsEqual(
+            '/1.2.3.txt',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/1.2.3.txt')
+
+    def test_rule_with_slash(self):
+        self.assertDestinationsEqual(
+            '/',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/index.html')
+
+    def test_file_in_folder_request(self):
+        self.assertDestinationsEqual(
+            '/png/1.png',
+            '/Users/irusland/Desktop/UrFU/'
+            'python/httpserver/tmp/pictures/1.png')
 
 
 if __name__ == '__main__':
