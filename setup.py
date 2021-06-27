30a04a641b3279565ff0f44ab34ad92eb246e1f0
Ruslan Sirazhetdinov <49832869+irusland@users.noreply.github.com>
2021-06-27 12:41:57 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     install_requires=required,
     cmdclass={
         'verify': VerifyVersionCommand,
